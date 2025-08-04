7d0790f8565b11dc7e23cbbf07531eddd3cff4b9
cchi0115 <cchi0115@student.monash.edu>
2025-08-04 19:38:10 +1000

    w2 efolio

---
+++ src/components/MyName.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="name-display">Hello, {{ name }}!</div>
+</template>
+
+<script setup>
+const name = 'ChunHeng'
+</script>
+
+<style scoped>
+.name-display {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 1rem;
+}
+</style>
